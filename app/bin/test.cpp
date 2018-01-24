5a89116c6b1e7e67abdd3defb557648d7f46c45c
Stack <stackos@qq.com>
2018-01-24 11:17:11 +0800

    test calling vertex shader.

--- app/bin/test.cpp
+++ app/bin/test.cpp
@@ -1,9 +1,81 @@
-#include <string.h>
+#define DLL_EXPORT extern "C" _declspec(dllexport)
+#define uniform
+#define attribute
+#define varying
 
-extern "C"
+struct vec4
 {
-    _declspec(dllexport) void hello(char* str, int size)
+    union
     {
-        strncpy(str, "hello world!", size);
+        struct
+        {
+            float x;
+            float y;
+            float z;
+            float w;
+        };
+
+        struct
+        {
+            float r;
+            float g;
+            float b;
+            float a;
+        };
+    };
+
+    vec4(float x = 0, float y = 0, float z = 0, float w = 0):
+        x(x),
+        y(y),
+        z(z),
+        w(w)
+    {
     }
+
+    vec4 operator*(const vec4& right) const
+    {
+        return vec4(x * right.x, y * right.y, z * right.z, w * right.w);
+    }
+};
+
+vec4 gl_Position;
+
+//
+// shader begin
+//
+uniform vec4 u_color;
+
+attribute vec4 a_position;
+attribute vec4 a_color;
+
+varying vec4 v_color;
+
+DLL_EXPORT
+void main()
+{
+    gl_Position = a_position;
+    v_color = a_color * u_color;
 }
+//
+// shader end
+//
+
+#define VS_VAR_SETTER(type, var) \
+    DLL_EXPORT void set_##var(void* p) \
+    { \
+        var = *(type*) p; \
+    }
+#define VS_VAR_GETTER(type, var) \
+    DLL_EXPORT void* get_##var() \
+    { \
+        return &var; \
+    }
+#define VS_UNIFORM_SETTER VS_VAR_SETTER
+#define VS_ATTRIBUTE_SETTER VS_VAR_SETTER
+#define VS_VARYING_GETTER VS_VAR_GETTER
+
+VS_UNIFORM_SETTER(vec4, u_color)
+VS_ATTRIBUTE_SETTER(vec4, a_position)
+VS_ATTRIBUTE_SETTER(vec4, a_color)
+VS_VAR_GETTER(vec4, gl_Position)
+VS_VARYING_GETTER(vec4, v_color)
--- app/src/AppShader.cpp
+++ app/src/AppShader.cpp
@@ -54,30 +54,84 @@ void exec_cmd(const std::string& path, const std::string& exe, const std::string
     }
 }
 
+struct vec4
+{
+    union
+    {
+        struct
+        {
+            float x;
+            float y;
+            float z;
+            float w;
+        };
+
+        struct
+        {
+            float r;
+            float g;
+            float b;
+            float a;
+        };
+    };
+
+    vec4(float x = 0, float y = 0, float z = 0, float w = 0):
+        x(x),
+        y(y),
+        z(z),
+        w(w)
+    {
+    }
+};
+
 class Renderer
 {
 public:
     Renderer()
     {
-        std::string cl_dir = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.12.25827\\bin\\Hostx64\\x64";
+        //std::string vs_path = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community";
+        std::string vs_path = "D:\\Program\\VS2017";
+
+        std::string cl_dir = vs_path + "\\VC\\Tools\\MSVC\\14.12.25827\\bin\\Hostx64\\x64";
         exec_cmd(cl_dir, "cl.exe", "/c test.cpp "
-            "/I \"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.12.25827\\include\" "
+            "/I " + vs_path + "\"\\VC\\Tools\\MSVC\\14.12.25827\\include\" "
             "/I \"C:\\Program Files (x86)\\Windows Kits\\10\\Include\\10.0.16299.0\\ucrt\"");
         exec_cmd(cl_dir, "link.exe", "/dll test.obj /OUT:test.dll "
-            "/LIBPATH:\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Community\\VC\\Tools\\MSVC\\14.12.25827\\lib\\x64\" "
+            "/LIBPATH:" + vs_path + "\"\\VC\\Tools\\MSVC\\14.12.25827\\lib\\x64\" "
             "/LIBPATH:\"C:\\Program Files (x86)\\Windows Kits\\10\\lib\\10.0.16299.0\\um\\x64\" "
             "/LIBPATH:\"C:\\Program Files (x86)\\Windows Kits\\10\\lib\\10.0.16299.0\\ucrt\\x64\"");
 
         auto dll = LoadLibrary("test.dll");
         if (dll)
         {
-            typedef void(*Func)(char*, int);
-            Func hello = (Func) GetProcAddress(dll, "hello");
-            if (hello)
+            typedef void*(*VSVarGetter)();
+            typedef void(*VSVarSetter)(void*);
+            typedef void(*VSMain)();
+
+            VSVarSetter set_uniform_color = (VSVarSetter) GetProcAddress(dll, "set_u_color");
+            VSVarSetter set_attr_position = (VSVarSetter) GetProcAddress(dll, "set_a_position");
+            VSVarSetter set_attr_color = (VSVarSetter) GetProcAddress(dll, "set_a_color");
+            VSMain vs_main = (VSMain) GetProcAddress(dll, "main");
+            VSVarGetter get_gl_position = (VSVarGetter) GetProcAddress(dll, "get_gl_Position");
+            VSVarGetter get_varying_color = (VSVarGetter) GetProcAddress(dll, "get_v_color");
+
+            vec4 gl_position;
+            vec4 v_color;
+
+            if (vs_main)
             {
-                char buffer[1024];
-                hello(buffer, 1024);
-                MessageBox(NULL, buffer, "", MB_OK);
+                vec4 ucolor(2, 2, 2, 2);
+                vec4 apos(0, 0, 0, 1);
+                vec4 acolor(3, 3, 3, 3);
+
+                set_uniform_color(&ucolor);
+                set_attr_position(&apos);
+                set_attr_color(&acolor);
+
+                vs_main();
+
+                gl_position = *(vec4*) get_gl_position();
+                v_color = *(vec4*) get_varying_color();
             }
             FreeLibrary(dll);
         }
